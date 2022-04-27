0c840026e5e01d54967c483f2ad9d0b88c4516c5
Arnas Vaicekauskas <arnasvaic7@gmail.com>
2022-04-27 23:36:11 +0300

    flight reading/finding

---
+++ include/flights.h
@@ -0,0 +1,40 @@
+#ifndef FLIGHTS_H
+#define FLIGHTS_H
+
+// System headers
+#include <limits.h>
+#include <stdio.h>
+
+// My headers
+#include <graph.h>
+#include <memory.h>
+
+// Longest city name in the world is 85 characters
+#define MAX_CITY_NAME_LEN 128
+
+typedef struct flight_t {
+    double leave;
+    double duration;
+    char *orig;
+    char *dest;
+} flight_t;
+
+/// Free flight object
+/// @param ptr pointer to flight
+/// @return nothing
+void free_flight(void *ptr);
+
+/// Read flight data from file and store it in a graph
+/// @param file input file
+/// @return graph containing flight info
+graph_t* read_flights(const char* file);
+
+/// Returns the shortest flight from city c1 to city c2
+/// if no such flight exists returns NULL
+/// @param g graph of flights
+/// @param c1 name of origin city
+/// @param c2 name of destination city
+/// @return pointer to shortest flight 
+flight_t* get_shortest_flight(graph_t *g, const char *c1, const char *c2);
+
+#endif /* FLIGHTS_H */
