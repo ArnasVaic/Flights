948b5b5e7f6ca685f043cd3ffbab511ea4be960f
Arnas Vaicekauskas <arnasvaic7@gmail.com>
2022-04-29 20:08:03 +0300

    Changed node struct name
    Now node_t is called list_node_t.

--- include/list.h
+++ include/list.h
@@ -6,23 +6,25 @@
 #include <string.h>
 #include <assert.h>
 
-// My headers
-#include <mem.h>
+#include <stdio.h>
 
-typedef struct node_t {
+// My headers
+#include <mymem.h>
+
+typedef struct list_node_t {
     void *data;
-    struct node_t *next;
-} node_t;
+    struct list_node_t *next;
+} list_node_t;
 
 typedef struct list_t {
-    node_t *head;
-    node_t *tail;
+    list_node_t *head;
+    list_node_t *tail;
     size_t size;
 } list_t;
 
 /// Creates a list
 /// @return list object
-list_t create_list();
+list_t* create_list();
 
 /// Adds an element to the list_t
 /// @param list pointer to list_t object
@@ -41,7 +43,7 @@ void* pop_front(list_t *list);
 /// @param list pointer to list_t object
 /// @param index index of desired element
 /// @return pointer to the element
-void* list_get(list_t list, int index);
+void* list_get(list_t *list, int index);
 
 /// Frees elements contained by list_t
 /// @param list pointer to list_t object
--- src/list.c
+++ src/list.c
@@ -1,10 +1,10 @@
 #include <list.h>
 
-node_t* create_node(void *data, size_t size) {
+list_node_t* create_list_node(void *data, size_t size) {
     assert(data);
     assert(size > 0);
 
-    node_t *node = (node_t*) __malloc(sizeof(node_t));
+    list_node_t *node = (list_node_t*) __malloc(sizeof(list_node_t));
     assert(node);
 
     node->data = __malloc(size);
@@ -16,19 +16,23 @@ node_t* create_node(void *data, size_t size) {
     return node;
 }
 
-void free_node(node_t *node, free_func_t f) {
+void free_list_node(list_node_t *node, free_func_t f) {
     f(node->data);
+    __free(node->data);
     __free(node);
 }
 
-list_t create_list() {
-    list_t list = {NULL, NULL, 0};
+list_t* create_list() {
+    list_t *list = __malloc(sizeof(list_t));
+    list->head = NULL;
+    list->tail = NULL;
+    list->size = 0;
     return list;
 }
 
 void push_back(list_t *list, void *data, size_t size) {
     assert(list);
-    node_t *node = create_node(data, size);
+    list_node_t *node = create_list_node(data, size);
     if(!list->head && !list->tail) {
         // Set head if this is the first node
         list->head = node; 
@@ -46,7 +50,7 @@ void* pop_front(list_t *list) {
     if(list->size == 0) return NULL;
     // Save head fields
     void *data = list->head->data;
-    node_t *next = list->head->next;
+    list_node_t *next = list->head->next;
     __free(list->head);
     // Change tail if this was the last node
     if(list->head == list->tail) list->tail = NULL; 
@@ -55,11 +59,11 @@ void* pop_front(list_t *list) {
     return data;
 }
 
-void* list_get(list_t list, int index) {
+void* list_get(list_t* list, int index) {
     assert(index >= 0);
-    assert(index < list.size);
+    assert(index < list->size);
 
-    node_t *it = list.head;
+    list_node_t *it = list->head;
     for(int i = 0; i < index; ++i) {
         it = it->next;
     }
@@ -68,11 +72,13 @@ void* list_get(list_t list, int index) {
 
 void free_list(list_t *list, free_func_t f) {
     assert(list);
-    if(list->size == 0) return;
-    node_t *it = list->head;
-    while(it) {
-        node_t *temp = it;
-        it = it->next;
-        free_node(temp, f);
+    if(list->size != 0) { 
+        list_node_t *it = list->head;
+        while(it) {
+            list_node_t *temp = it;
+            it = it->next;
+            free_list_node(temp, f);
+        }
     }
+    __free(list);
 }
